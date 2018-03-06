92def94e31e50eec3e9b1e580aa04c29365b472e
Zhang Keliang <keliang@fudan.edu.cn>
2018-03-06 14:22:53 +0800

    Add MySQL combination

---
+++ combination/mysql/combination.py
@@ -0,0 +1,109 @@
+﻿# encoding=utf-8
+
+from util import *
+
+import mysql.connector
+import time
+
+# 准备原始数据
+logging.info('Prepare source data.')
+
+conn_src_1 = mysql.connector.connect(host='localhost', port=3306, user='root', password='123456', use_unicode=True)
+conn_src_2 = mysql.connector.connect(host='localhost', port=3306, user='root', password='123456', use_unicode=True)
+conn_dst = mysql.connector.connect(host='localhost', port=3306, user='root', password='123456', use_unicode=True)
+
+cursor_src_1 = conn_src_1.cursor()
+cursor_src_2 = conn_src_2.cursor()
+cursor_dst = conn_dst.cursor()
+
+# 清除遗留的原始数据
+logging.info('Delete old source data.')
+
+cursor_src_1.execute('DROP DATABASE IF EXISTS db_src_1;')
+conn_src_1.commit()
+
+cursor_src_2.execute('DROP DATABASE IF EXISTS db_src_2;')
+conn_src_2.commit()
+
+cursor_dst.execute('DROP DATABASE IF EXISTS db_dst;')
+conn_dst.commit()
+
+# 初始化原始数据
+logging.info('Init source data.')
+
+cursor_src_1.execute('CREATE DATABASE db_src_1 DEFAULT CHARSET utf8 COLLATE utf8_general_ci;')
+cursor_src_1.execute('USE db_src_1;')
+cursor_src_1.execute('CREATE TABLE user (id VARCHAR(20) PRIMARY KEY, name VARCHAR(20))')
+cursor_src_1.execute('INSERT INTO user (id, name) VALUES (%s, %s)', ['17400100001', 'Michael 1'])
+cursor_src_1.execute('INSERT INTO user (id, name) VALUES (%s, %s)', ['17400100002', 'Michael 2'])
+conn_src_1.commit()
+
+cursor_src_1.execute('CREATE TABLE user2 (id VARCHAR(20) PRIMARY KEY, name VARCHAR(20))')
+cursor_src_1.execute('INSERT INTO user2 (id, name) VALUES (%s, %s)', ['17400100001', 'Michael 1'])
+cursor_src_1.execute('INSERT INTO user2 (id, name) VALUES (%s, %s)', ['17400100002', 'Michael 2'])
+conn_src_1.commit()
+
+cursor_src_2.execute('CREATE DATABASE db_src_2 DEFAULT CHARSET utf8 COLLATE utf8_general_ci;')
+cursor_src_2.execute('USE db_src_2;')
+cursor_src_2.execute('CREATE TABLE user (id VARCHAR(20) PRIMARY KEY, name VARCHAR(20))')
+cursor_src_2.execute('INSERT INTO user (id, name) VALUES (%s, %s)', ['17400200001', 'Joey 1'])
+cursor_src_2.execute('INSERT INTO user (id, name) VALUES (%s, %s)', ['17400200002', 'Joey 2'])
+conn_src_2.commit()
+
+cursor_src_2.execute('CREATE TABLE user2 (id VARCHAR(20) PRIMARY KEY, name VARCHAR(20))')
+cursor_src_2.execute('INSERT INTO user2 (id, name) VALUES (%s, %s)', ['17400200001', 'Joey 1'])
+cursor_src_2.execute('INSERT INTO user2 (id, name) VALUES (%s, %s)', ['17400200002', 'Joey 2'])
+conn_src_2.commit()
+
+cursor_dst.execute('create database db_dst DEFAULT CHARSET utf8 COLLATE utf8_general_ci;')
+cursor_dst.execute('use db_dst;')
+cursor_dst.execute('create table user (id varchar(20) primary key, name varchar(20))')
+cursor_dst.execute('create table user2 (id varchar(20) primary key, name varchar(20))')
+conn_dst.commit()
+
+cursor_src_1.close()
+cursor_src_2.close()
+cursor_dst.close()
+
+conn_src_1.close()
+conn_src_2.close()
+conn_dst.close()
+
+# 开始合服
+conn_db_src_1 = mysql.connector.connect(host='localhost', port=3306, user='root', password='123456', database='db_src_1', use_unicode=True)
+conn_db_src_2 = mysql.connector.connect(host='localhost', port=3306, user='root', password='123456', database='db_src_2', use_unicode=True)
+conn_db_dst = mysql.connector.connect(host='localhost', port=3306, user='root', password='123456', database='db_dst', use_unicode=True)
+
+cursor_db_src_1 = conn_db_src_1.cursor()
+cursor_db_src_2 = conn_db_src_2.cursor()
+cursor_db_dst = conn_db_dst.cursor()
+
+tbl_names = list()
+tbl_names.append('user')
+tbl_names.append('user2')
+
+logging.info('')
+logging.info('checking before combination start...')
+for tbl_name in tbl_names:
+    check_tbl_before_combination(cursor_db_src_1, cursor_db_src_2, cursor_db_dst, conn_db_dst, tbl_name)
+logging.info('checking before combination end!!!')
+
+logging.info('')
+logging.info('combination start...')
+for tbl_name in tbl_names:
+    combinate_tbl_by_name(cursor_db_src_1, cursor_db_src_2, cursor_db_dst, conn_db_dst, tbl_name, False)
+logging.info('combination end!!!')
+
+logging.info('')
+logging.info('checking after combination start...')
+for tbl_name in tbl_names:
+    check_tbl_after_combination(cursor_db_src_1, cursor_db_src_2, cursor_db_dst, tbl_name)
+logging.info('checking after combination  end!!!')
+
+cursor_db_src_1.close()
+cursor_db_src_2.close()
+cursor_db_dst.close()
+
+conn_db_src_1.close()
+conn_db_src_2.close()
+conn_db_dst.close()
---
+++ combination/mysql/util.py
@@ -0,0 +1,150 @@
+﻿# encoding=utf-8
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG,
+                format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+                datefmt='%Y-%m-%d %H:%M:%S',
+                filename='combination.log',
+                filemode='w')
+				
+console = logging.StreamHandler()
+# console.setLevel(logging.DEBUG)
+console.setLevel(logging.INFO)
+#formatter = logging.Formatter('%(asctime)-s: %(filename)s[line:%(lineno)d] %(levelname)-s %(message)s')
+formatter = logging.Formatter('%(levelname)-s %(message)s')
+console.setFormatter(formatter)
+logging.getLogger('').addHandler(console)
+
+def check_combination(cursor_db_src_1, cursor_db_src_2, cursor_dst, tbl_name):	
+    counts_db_src_1 = get_tbl_counts(cursor_db_src_1, tbl_name)
+    counts_db_src_2 = get_tbl_counts(cursor_db_src_2, tbl_name)
+    counts_dst = get_tbl_counts(cursor_dst, tbl_name)
+	
+    logging.debug( 'counts_db_src_1 = %s' % (counts_db_src_1) )
+    logging.debug( 'counts_db_src_2 = %s' % (counts_db_src_2) )
+    logging.debug( 'counts_dst = %s' % (counts_dst) )
+	
+    if counts_db_src_1 + counts_db_src_2 != counts_dst:
+        return False, counts_db_src_1, counts_db_src_2, counts_dst
+		
+    return True, counts_db_src_1, counts_db_src_2, counts_dst
+
+def check_tbl_after_combination(cursor_db_src_1, cursor_db_src_2, cursor_db_dst, tbl_name):
+    logging.info( 'check table %s start...' % (tbl_name) )
+    
+    checked, counts_db_src_1, counts_db_src_2, counts_dst = check_combination(cursor_db_src_1, cursor_db_src_2, cursor_db_dst, tbl_name)
+    
+    if not checked:
+        logging.error( 'combination table %s end failed (%d + %d != %d)!!!' % (tbl_name, counts_db_src_1, counts_db_src_2, counts_dst) )
+    else:
+        logging.info( 'combination table %s end successfully (%d + %d = %d)!!!' % (tbl_name, counts_db_src_1, counts_db_src_2, counts_dst) )
+	
+	
+    if (not checked):
+        logging.error( 'check table %s end failed!!!' % (tbl_name) )
+		
+        return False
+	
+    logging.info( 'check table %s end successfully!!!' % (tbl_name) )
+    return True
+
+def check_tbl_before_combination(cursor_db_src_1, cursor_db_src_2, cursor_db_dst, conn_db_dst, tbl_name):
+    logging.info( 'check table %s start...' % (tbl_name) )
+    
+    fields_db_src_1 = get_tbl_fields(cursor_db_src_1, tbl_name)
+    logging.debug( 'fields_db_src_1 = %s' % ( ", ".join(fields_db_src_1) ) )
+    
+    fields_db_src_2 = get_tbl_fields(cursor_db_src_2, tbl_name)
+    logging.debug( 'fields_db_src_2 = %s' % ( ", ".join(fields_db_src_2) ) )
+    
+    checked = check_tbl_by_fields(fields_db_src_1, fields_db_src_2)
+    if (not checked):
+        logging.error( 'check table %s end failed!!!' % (tbl_name) )
+		
+        return False
+	
+    logging.info( 'check table %s end successfully!!!' % (tbl_name) )
+    return True
+
+def check_tbl_by_fields(fields1, fields2):
+    logging.debug( type(fields1) )
+    logging.debug( type(fields2) )
+	
+    if ( ( not isinstance(fields1, list) ) and ( not isinstance(fields1, tuple) ) ):
+        return False
+	
+    if ( ( not isinstance(fields2, list) ) and ( not isinstance(fields2, tuple) ) ):
+        return False
+	
+    if (fields1 != fields2):
+        return False
+	
+    return True
+
+def combinate_tbl_by_name(cursor_db_src_1, cursor_db_src_2, cursor_db_dst, conn_db_dst, tbl_name, check):
+    logging.info( 'combination table %s start...' % (tbl_name) )
+    
+    fields_db_src_1 = get_tbl_fields(cursor_db_src_1, tbl_name)
+    logging.debug( 'fields_db_src_1 = %s' % ( ", ".join(fields_db_src_1) ) )
+    
+    fields_db_src_2 = get_tbl_fields(cursor_db_src_2, tbl_name)
+    logging.debug( 'fields_db_src_2 = %s' % ( ", ".join(fields_db_src_2) ) )
+    
+    checked = check_tbl_by_fields(fields_db_src_1, fields_db_src_2)
+    if (not checked):
+        logging.error( 'Fields of table %s check failed!!!' % (tbl_name) )
+    
+    combinate_tbl_by_fields(cursor_db_src_1, cursor_db_dst, tbl_name, fields_db_src_1)
+    combinate_tbl_by_fields(cursor_db_src_2, cursor_db_dst, tbl_name, fields_db_src_2)
+    conn_db_dst.commit()
+	
+    if not check:
+        logging.error( 'combination table %s end' % (tbl_name) )
+    
+    if check:
+        checked, counts_db_src_1, counts_db_src_2, counts_dst = check_combination(cursor_db_src_1, cursor_db_src_2, cursor_db_dst, tbl_name)
+        
+        if not checked:
+            logging.error( 'combination table %s end failed (%d + %d != %d)!!!' % (tbl_name, counts_db_src_1, counts_db_src_2, counts_dst) )
+        else:
+            logging.info( 'combination table %s end successfully (%d + %d = %d)!!!' % (tbl_name, counts_db_src_1, counts_db_src_2, counts_dst) )
+	
+def combinate_tbl_by_fields(cursor_src, cursor_dst, tbl_name, fields):
+    sqlQuery = "SELECT " + ", ".join(fields) + " FROM " + tbl_name + ";"
+    logging.debug( 'sqlQuery = %s' % (sqlQuery) )
+	
+    points = list()
+    for i in range( len(fields) ):
+        points.append('%s')	
+    sqlInsert = "INSERT INTO  " + tbl_name + " (" + ", ".join(fields) + ") VALUES (" + ", ".join(points) + ")"
+    logging.debug( 'sqlInsert = %s' % (sqlInsert) )
+	
+    cursor_src.execute(sqlQuery)
+    rows = cursor_src.fetchall()
+    for row in rows:
+        logging.debug('type(row) = %s' % (type(row)))
+        logging.debug('row = %s' % (", ".join(row)))
+        cursor_dst.execute(sqlInsert, row)
+
+def get_tbl_counts(cursor, tbl_name):
+    cursor.execute("SELECT COUNT(*) FROM " + tbl_name)
+    rows = cursor.fetchall()
+    counts = rows[0][0]
+    logging.debug('type(counts) = %s' % (type(counts)))
+    logging.debug('counts = %s' % (counts))
+    
+    return counts
+	
+def get_tbl_fields(cursor, tbl_name):
+    fields = list()
+    
+    cursor.execute('SHOW COLUMNS FROM ' + tbl_name)
+    rows = cursor.fetchall()
+    
+    for row in rows:
+        fields.append( row[0] )
+    
+    fields.sort()
+	
+    return fields
